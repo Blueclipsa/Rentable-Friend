4df397132971d33b2f6ee848ca619498ef63c6b3
Gerald Harris <wheatleyiscool@gmail.com>
2017-10-23 11:31:24 -0500

    Initial Commit

---
+++ Rent A Friend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rent A Friend
+//
+//  Created by Bluejay on 10/23/17.
+//  Copyright © 2017 Bluejay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
